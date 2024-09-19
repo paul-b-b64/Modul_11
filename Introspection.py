39bbf30568c5fae8cf2b8f14ce5786d6f2214147
paul-b-b64 <belousko@inbox.ru>
2024-09-19 15:01:53 +0300

    Introspection

---
+++ Introspection.py
@@ -0,0 +1,14 @@
+def introspection_info(obj):
+    info = {'type': type(obj).__name__, 'attributes': [], 'methods': [], 'module': __name__}
+    for attr in dir(obj):
+        if callable(getattr(obj, attr)):
+            info['methods'].append(attr)
+        else:
+            info['attributes'].append(attr)
+    return info
+
+number_info = introspection_info(42)
+print(number_info)
+
+
+
