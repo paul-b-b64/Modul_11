b07a67acf127531d0d43f070d48b93991bacf65a
paul-b-b64 <belousko@inbox.ru>
2024-09-24 14:22:03 +0300

    11_1

--- Modul_11_1.py
+++ Modul_11_1.py
@@ -1,8 +1,20 @@
-from pprint import pprint
-
 import pandas as pd
-import numpy as np
+from matplotlib import pyplot as plt
 
-Df = pd.read_excel('Test.xlsx')
+# загружаем таблицу из файла Excel
+with pd.ExcelFile('Test.xlsx') as excel:
+    tabl = pd.read_excel(excel, header=0)
+    print(tabl)
 
-print(Df)
+# добавил столбцы
+tabl['Цвет глаз'] = ['серый', 'голубой', 'малиновый', 'красный', 'зеленый', 'прозрачный']
+print(tabl)
+tabl['Возраст'] = [33, 23, 34, 34, 25, 45]
+print(tabl)
+# выводим столбец с телефонами
+print(tabl['Телефон'])
+# выводим вторую запись
+print(tabl.iloc[1])
+# печатаем график возраста клиентов
+sf = tabl.plot(x='Nпп', y=['Возраст'])
+plt.show()
